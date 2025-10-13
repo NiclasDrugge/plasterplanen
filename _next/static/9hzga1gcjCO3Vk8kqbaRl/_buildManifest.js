7eb66994c41c3d12650a7cff677eb23138884661
Niclas Drugge <niclas_drugge@hotmail.com>
2025-10-13 20:01:35 +0200

    Updates

---
+++ _next/static/9hzga1gcjCO3Vk8kqbaRl/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/9a1bf10eb0790a15.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/plasterplanen//_next/:path+",
+        "destination": "/plasterplanen/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/b22812ad3079f0e4.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/321c4d7b3c5e0856.js",
+  "static/chunks/turbopack-6163dc8f832818cc.js"
+])
